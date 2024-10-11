d4dd03c008264cade2c2a95c189303c2bed6f787
Illia Sakharau <sakharovia0204@gmail.com>
2024-10-11 09:56:04 +0300

    feat: add OutputSectionFooter component

--- src/components/outputs-section/OutputSection.js
+++ src/components/outputs-section/OutputSection.js
@@ -1,5 +1,6 @@
 import Surface from "../surface/Surface.js";
 import OutputSectionContent from "./components/OutputSectionContent.js";
+import OutputSectionFooter from "./components/OutputSectionFooter.js";
 import OutputSectionHeader from "./components/OutputSectionHeader.js";
 
 export default class OutputSection {
@@ -9,8 +10,7 @@ export default class OutputSection {
 
     const header = new OutputSectionHeader().draw();
     const content = new OutputSectionContent().draw();
-    const footer = document.createElement('div');
-    footer.innerText = 'Footer';
+    const footer = new OutputSectionFooter().draw();
 
     this.inner.append(header, content, footer);
     this.component = new Surface(this.inner).draw();
